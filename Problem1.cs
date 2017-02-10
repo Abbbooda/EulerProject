4420437c7654d5e1f59dd8de0484981641fd7dc9
Abbbooda <elkhaloufiabderrahman@gmail.com>
2017-02-10 10:58:49 +0000

    First Problem

---
+++ Problem1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject
+{
+    class Problem1
+    {
+        public int Mult3and5(int number, int max)
+        {
+            int result = 0;
+            result = number * (max * (max + 1)) / 2;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem1 p1 = new Problem1();
+            //int result = p1.Mult3and5(3, 333) + p1.Mult3and5(5, 199) - p1.Mult3and5(15, 66);
+            //Console.WriteLine(result);
+            //Console.ReadKey();
+        }
+    }
+}
